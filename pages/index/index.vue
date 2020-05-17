4a55ab4430a743d02e5fbc8612060f0b44a7d7f0
liyuanz <13419866507@163.com>
2020-05-17 10:45:07 +0800

    update

---
+++ pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+			
+		</view>
+		<navigator  url="viewcontainer/view">
+			<button type="default">视图组件</button>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+            title:"点击进入对应组件界面"
+			}
+		},
+		onLoad(option) {
+			console.log(option.id);
+			console.log(option.name);
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/index/viewcontainer/view.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+			<tagname property="value">
+				content
+			</tagname>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
